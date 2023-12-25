b19bf773fca7506d1754d3287e9fc3cad8b9e005
Kungawo <tenzinjangchuk@Kungawo.local>
2023-12-25 16:23:11 -0600

    first commit

---
+++ practice.py
@@ -0,0 +1,8 @@
+# set my_number to 42. Set my_name to your name. now swap my_number into my_name and vice versa.
+my_number = 42
+my_name = "tenz"
+print(my_number, my_name)
+temp = my_number
+my_number = my_name
+my_name = temp
+print(my_number, my_name)
