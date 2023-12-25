a587f5335bd4e868cdc050621efef88b09c571ed
Kungawo <tenzinjangchuk@Kungawo.local>
2023-12-25 16:53:10 -0600

    adding algos

--- practice.py
+++ practice.py
@@ -6,3 +6,14 @@ temp = my_number
 my_number = my_name
 my_name = temp
 print(my_number, my_name)
+
+# print -52 t0 1066 through For loop
+for i in range(-52,1066):
+    print(i)
+
+# create a function be_cheerful(). Within it console.log string "good morning!" Call it 98 times
+def be_cheerful():
+    for i in range(0,99):
+        i = print("good morning!")
+    return i
+be_cheerful()
